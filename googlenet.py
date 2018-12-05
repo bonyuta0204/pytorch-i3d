eacce0a179d20659ad5081c9b72ca0ffb38465bf
Yuta Nakamura (smith) <nakamurayuta0204@gmail.com>
2018-12-05 16:58:57 +0900

    add pytorch_caffe module

--- googlenet.py
+++ googlenet.py
@@ -1,1 +1,14 @@
-import pytorch_caffe
+import torch
+
+import caffe
+from pytorch_caffe import caffenet
+
+
+def forward_pytorch(protofile, weightfile, image):
+    net = caffenet.CaffeNet(protofile)
+    print(net)
+    net.load_weights("models/googlenet/bvlc_googlenet.caffemodel")
+    net.eval()
+    image = torch.from_numpy(image)
+    blobs = net(image)
+    return blobs, net.models
